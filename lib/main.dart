d9ea4b7d37de6f4278d52c2a61a91a233806e833
sheldonholic <waghamaleshamika2013@gmail.com>
2022-09-14 00:34:46 +0530

    splashscreen added

--- lib/main.dart
+++ lib/main.dart
@@ -37,8 +37,19 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.white,
-        child: FlutterLogo(size: MediaQuery.of(context).size.height));
+        //Scolor: Colors.white,
+        child:
+            Image.network("https://img.icons8.com/color/144/000000/sprout.png"),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 250, 247, 250),
+              Color.fromARGB(255, 214, 245, 187)
+            ],
+            end: Alignment.bottomLeft,
+            begin: Alignment.topRight,
+          ),
+        ));
   }
 }
 
@@ -46,12 +57,200 @@ class SecondScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("GeeksForGeeks")),
-      body: Center(
-          child: Text(
-        "Home page",
-        textScaleFactor: 2,
-      )),
-    );
+        body: Padding(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 40),
+              Text(
+                "Crassula",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Living Coral, RattleSnake Plant",
+                style: TextStyle(
+                    fontSize: 15, color: Color.fromARGB(255, 107, 104, 104)),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                        width: 100.0,
+                        height: 150.0,
+                        child: Image.network(
+                            "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80")),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 229, 238, 208),
+                        border: Border.all(
+                            width: 5,
+                            color: Color.fromARGB(255, 229, 238, 208)),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      width: 150.0,
+                      height: 150.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 100.0,
+                            height: 100.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Row(
+                                  children: [
+                                    Container(
+                                        child: Image.network(
+                                            "https://img.icons8.com/ios/20/000000/water.png")),
+                                    Container(
+                                      child: Text(
+                                        "Watering",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("Moderate"),
+                                Text("1-2 times/week")
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 229, 238, 208),
+                        border: Border.all(
+                            width: 5,
+                            color: Color.fromARGB(255, 229, 238, 208)),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      width: 150.0,
+                      height: 150.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 100.0,
+                            height: 100.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Row(
+                                  children: [
+                                    Container(
+                                        child: Image.network(
+                                            "https://img.icons8.com/external-phatplus-solid-phatplus/20/000000/external-humidity-weather-phatplus-solid-phatplus.png")),
+                                    Container(
+                                      child: Text(
+                                        " Humidity",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("Moderate"),
+                                Text("sparse spraying")
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 229, 238, 208),
+                        border: Border.all(
+                            width: 5,
+                            color: Color.fromARGB(255, 229, 238, 208)),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      width: 150.0,
+                      height: 150.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            width: 100.0,
+                            height: 100.0,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Row(
+                                  children: [
+                                    Container(
+                                        child: Image.network(
+                                            "https://img.icons8.com/ios/20/000000/water.png")),
+                                    Container(
+                                      child: Text(
+                                        "Watering",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("Moderate"),
+                                Text("1-2 times/week")
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+      ),
+    ));
   }
 }

4cf7eaea467f605b84fb133ef6398f87d563dbca
ninad <ninadsrao@gmail.com>
2022-10-03 23:41:18 -0700

    :zap:  Load page without live data

--- lib/screens/PlantScreen.dart
+++ lib/screens/PlantScreen.dart
@@ -40,8 +40,311 @@ class _PlantScreenState extends State<PlantScreen> {
           stream: client.updates,
           builder: (context, snapshot) {
             if (!snapshot.hasData)
+              // var result = 0;
+
               // Iska end mein dekhte
-              return Center();
+              return Padding(
+                padding: const EdgeInsets.all(
+                  20,
+                ),
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      const Text(
+                        "Crassula",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Living Coral, RattleSnake Plant",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Color.fromARGB(255, 107, 104, 104),
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          InkWell(
+                            onTap: () {},
+                            child: SizedBox(
+                                width: 100.0,
+                                height: 150.0,
+                                child: Image.network(
+                                    "https://images.unsplash.com/photo-1509423350716-97f9360b4e09?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80")),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 229, 238, 208),
+                                border: Border.all(
+                                  width: 5,
+                                  color:
+                                  const Color.fromARGB(255, 229, 238, 208),
+                                ),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(
+                                    50,
+                                  ),
+                                ),
+                              ),
+                              width: 150.0,
+                              height: 150.0,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  SizedBox(
+                                    width: 100.0,
+                                    height: 100.0,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Row(
+                                          children: <Widget>[
+                                            Row(
+                                              children: [
+                                                Text(
+                                                  "0",
+                                                  style: const TextStyle(
+                                                    fontSize: 35,
+                                                    fontWeight: FontWeight.bold,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  " ℃",
+                                                  style: const TextStyle(
+                                                    fontSize: 20,
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        const Text("Temperature"),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 229, 238, 208),
+                                border: Border.all(
+                                  width: 5,
+                                  color:
+                                  const Color.fromARGB(255, 229, 238, 208),
+                                ),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(
+                                    50,
+                                  ),
+                                ),
+                              ),
+                              width: 150.0,
+                              height: 150.0,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  SizedBox(
+                                    width: 100.0,
+                                    height: 100.0,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Row(
+                                          children: [
+                                            Image.network(
+                                                "https://img.icons8.com/external-those-icons-lineal-those-icons/30/000000/external-humidity-weather-those-icons-lineal-those-icons.png"),
+                                            Text(
+                                              "0",
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text("--")
+                                          ],
+                                        ),
+                                        const Text("Humidity"),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          InkWell(
+                            onTap: () {},
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: const Color.fromARGB(255, 229, 238, 208),
+                                border: Border.all(
+                                  width: 5,
+                                  color:
+                                  const Color.fromARGB(255, 229, 238, 208),
+                                ),
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(
+                                    50,
+                                  ),
+                                ),
+                              ),
+                              width: 150.0,
+                              height: 150.0,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  SizedBox(
+                                    width: 100.0,
+                                    height: 100.0,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                      MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Row(
+                                          children: [
+                                            Image.network(
+                                                "https://img.icons8.com/ios/30/000000/water.png"),
+                                            Text(
+                                              "0",
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(
+                                          height: 10,
+                                        ),
+                                        Column(
+                                          children: [
+                                            Text("--")
+                                          ],
+                                        ),
+                                        const Text("Soil Moisture"),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(
+                              25,
+                            ),
+                            child: Column(
+                              children: [
+                                const Text(
+                                  "Description",
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Container(
+                                  width:
+                                  MediaQuery.of(context).size.width * 0.75,
+                                  padding: const EdgeInsets.all(
+                                    10,
+                                  ),
+                                  child: const Text(
+                                    "Most crassula plants grown as houseplants originated from the eastern cape of South Africa. If you have the proper climate, the plants look terrific in the garden, but all look just as excellent indoors.",
+                                    //overflow: TextOverflow.fade,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Text(
+                        "Plant Health",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 150,
+                        child: SfCartesianChart(
+                          primaryXAxis: CategoryAxis(),
+                          palette: const <Color>[
+                            Color.fromARGB(255, 116, 214, 74),
+                          ],
+                          // Chart title
+                          // title: ChartTitle(text: 'Plant Health'),
+                          // Enable legend
+                          legend: Legend(isVisible: false),
+                          // Enable tooltip
+                          //tooltipBehavior: _tooltipBehavior,
+                          series: <LineSeries<SalesData, String>>[
+                            LineSeries<SalesData, String>(
+                                dataSource: <SalesData>[
+                                  SalesData('-5hr', 2.0),
+                                  SalesData('-4hr',2.0),
+                                  SalesData('-3hr',2.0),
+                                  SalesData('-2hr', 2.0),
+                                  SalesData('now', 2.0)
+                                ],
+                                xValueMapper: (SalesData sales, _) =>
+                                sales.year,
+                                yValueMapper: (SalesData sales, _) =>
+                                sales.sales,
+                                // Enable data label
+                                dataLabelSettings:
+                                const DataLabelSettings(isVisible: false))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              );
             else {
               final mqttReceivedMessages =
                   snapshot.data as List<MqttReceivedMessage<MqttMessage?>>?;
